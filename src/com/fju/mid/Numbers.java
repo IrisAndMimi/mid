b80b605d6d7439bdbbde7fd569936f150292da1f
IrisAndMimi <my1ris227@gmail.com>
2017-11-14 14:11:55 +0800

    Numbers

---
+++ src/com/fju/mid/Numbers.java
@@ -0,0 +1,17 @@
+package com.fju.mid;
+
+import java.util.Scanner;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		int m = Integer.parseInt(line);
+		for(int n = m; n>0 ;n--){
+			
+			
+		}
+	}
+
+}
